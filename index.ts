7b713e09bf01dd905d4c2faa7477b771254d9237
戴嘉华 <daijiahua@qunjielong.com>
2019-07-16 18:15:52 +0800

    feat: make shortest path to target

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import Graph = require("node-dijkstra")
+
 interface Rectangle {
   left: number,
   top: number,
@@ -45,7 +47,7 @@ export const getShortestPath = (
     y: centerTopPoint.y + (centerBottomPoint.y - centerTopPoint.y) / 2,
   }
 
-  let crossPoints = getAllCrossPointsByPoints(
+  const crucialPoints = [
     fromPoint,
     toPoint,
     topPoint,
@@ -53,34 +55,76 @@ export const getShortestPath = (
     centerLeftPoint,
     centerRightPoint,
     centerCrossPoint,
-  )
+  ]
   
-  crossPoints = removeOverlapPoints(crossPoints, fromRect)
-  crossPoints = removeOverlapPoints(crossPoints, toRect)
-  const routerTable = makeRouterTableByPoints(crossPoints)
-  return dijkstraPathByRouterTable(fromPoint, toPoint, routerTable)
+  const graph = makeGraphByPoints(toRect, fromRect, crucialPoints, centerCrossPoint)
+  return dijkstraPathByGraph(fromPoint, toPoint, graph)
 }
 
-const getAllCrossPointsByPoints = (...points: Point[]): Point[] => {
-  const xSet = new Set(points.map(p => p.x))
-  const ySet = new Set(points.map(p => p.y))
-  const ret: Point[] = []
-  xSet.forEach((x) => {
-    ySet.forEach((y) => {
-      ret.push({ x, y })
+const makeGraphByPoints = (rect1: Rectangle, rect2: Rectangle, points: Point[], cross: Point): Graph => {
+  const xSet = Array.from(new Set(points.map(p => p.x * 1))).sort((a, b) => a - b)
+  const ySet = Array.from(new Set(points.map(p => p.y * 1))).sort((a, b) => a - b)
+
+  const map = Array
+    .from({ length: xSet.length })
+    .map(() => Array.from({ length: ySet.length }).fill(0))
+
+  const graph = new Graph()
+
+  const checkPointPass = (i: number, j: number): boolean => {
+    const x = xSet[i]
+    const y = ySet[j]
+    const point = { x, y }
+    /** 移除无用路径 */
+    if (
+      isPointInRect(point, rect1) ||
+      isPointInRect(point, rect2)
+    ) {
+      map[i][j] = -1
+      return false
+    }
+    map[i][j] = 1
+    return true
+  }
+
+  xSet.forEach((x, i) => {
+    ySet.forEach((y, j) => {
+      if (!checkPointPass(i, j)) { return }
+      const currentPath = {}
+      const currentPathName = `(${x},${y})`
+      const dist = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+      dist.forEach(([iDist, jDist]) => {
+        const ip = i + iDist
+        if (ip < 0 || ip >= xSet.length) { return }
+
+        const jp = j + jDist
+        if (jp < 0 || jp >= ySet.length || (iDist === 0 && jDist === 0)) { return }
+
+        if (map[ip][jp] === 0) {
+          checkPointPass(ip, jp)
+        }
+
+        if (map[ip][jp] === -1) {
+          return
+        }
+
+        let tx = xSet[ip]
+        let ty = ySet[jp]
+
+        const center = { x: x + (tx - x) / 2, y: y + (ty - y) /2 }
+        if (isPointInRect(center, rect1) || isPointInRect(center, rect2)) {
+          return
+        }
+
+        const tPathName = `(${tx},${ty})`
+        // 交叉点优先经过
+        const isCenter = (tx === cross.x && ty === cross.y)
+        currentPath[tPathName] = Math.abs(tx - x) + Math.abs(ty - y) + (isCenter ? -1 : 0)
+      })
+      graph.addNode(currentPathName, currentPath)
     })
   })
-  return ret
-}
-
-const removeOverlapPoints = (points: Point[], rect1: Rectangle): Point[] => {
-  const ret: Point[] = []
-  points.forEach((p: Point) => {
-    if (!isPointInRect(p, rect1)) {
-      ret.push(p)
-    }
-  })
-  return ret
+  return graph
 }
 
 const isPointInRect = (point: Point, rect: Rectangle): boolean => {
@@ -90,12 +134,8 @@ const isPointInRect = (point: Point, rect: Rectangle): boolean => {
     point.y < (rect.top + rect.height)
 }
 
-const makeRouterTableByPoints = (points: Point[]) => {
-  // TODO
-}
-
-const dijkstraPathByRouterTable = (from: Point, to: Point, routerTable) => {
-  // TODO
+const dijkstraPathByGraph = (from: Point, to: Point, graph: Graph) => {
+ return graph.path(`(${from.x},${from.y})`, `(${to.x},${to.y})`)
 }
 
 const getRectPoints = (rect: Rectangle): Point[] => {
@@ -106,3 +146,20 @@ const getRectPoints = (rect: Rectangle): Point[] => {
   const d = { x: x1, y: y1 + h1 }
   return [a, b, c, d]
 }
+
+const test = () => {
+  return getShortestPath(
+    { left: 10, top: 10, width: 100, height: 100 },
+    { x: 10, y: 20 },
+    Direction.BOTTOM,
+
+    { left: 130, top: 130, width: 50, height: 100 },
+    { x: 180, y: 180 },
+    Direction.BOTTOM,
+  )
+}
+
+console.time("time")
+const path = test()
+console.timeEnd("time")
+console.log(path)
